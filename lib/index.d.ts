e2a3ab767507d81955865439d2182cad184f5462
takuma.katagiri <tioken12@gmail.com>
2019-12-12 15:29:12 +0900

    update dist

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,8 @@
 import Component from './component';
 import Mixin from './mixin';
 import Toolbar from './toolbar';
+import Quill from 'quill';
 export default Component;
+export { Quill };
 export { Mixin };
 export { Toolbar };
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ https://github.com/zenoamaro/react-quill
 import Component from './component';
 import Mixin from './mixin';
 import Toolbar from './toolbar';
+import Quill from 'quill';
 
 export default Component;
+export {Quill};
 export {Mixin};
 export {Toolbar};
